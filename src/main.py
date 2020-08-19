42f2cb2b5c0de67d02d0a7a59b7557e3ae572751
YuSawan <sawada.yuya.sr7@is.naist.jp>
2020-08-19 14:32:58 +0900

    main file

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from src.dataloader import DataLoader
+from src.preprocessing import Sequence
+
+if __name__ == '__main__':
+    chemdner_train_link = '../chemdner/full_type_data/conllform/train_conllform.txt'
+    chemdner_dev_link = '../chemdner/full_type_data/conllform/valid_conllform.txt'
+    chemdner_test_link = '../chemdner/full_type_data/conllform/test_conllform.txt'
+
+    leader = DataLoader(anno_format='conll')
+
+    x_train, y_train = leader.load_data(chemdner_train_link)
+    x_dev, y_dev = leader.load_data(chemdner_dev_link)
+    x_test, y_test = leader.load_data(chemdner_test_link)
+
+    model = Sequence(use_char=True)
+    model.fit(x_train=x_train, y_train=y_train, epochs=1, batch_size=10, shuffle=True)
